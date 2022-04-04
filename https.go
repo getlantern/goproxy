6c78c1bdfd0485abf9f7a27619e061ee6406bbd4
Alec Soltzen <83119415+soltzen@users.noreply.github.com>
2022-04-04 14:42:51 +0200

    [https] Add nil checks to copy methods

--- https.go
+++ https.go
@@ -319,6 +319,9 @@ func httpError(w io.WriteCloser, ctx *ProxyCtx, err error) {
 }
 
 func copyOrWarn(ctx *ProxyCtx, dst io.Writer, src io.Reader, wg *sync.WaitGroup) {
+	if dst == nil || src == nil {
+		return
+	}
 	if _, err := io.Copy(dst, src); err != nil {
 		ctx.Warnf("Error copying to client: %s", err)
 	}
@@ -326,6 +329,9 @@ func copyOrWarn(ctx *ProxyCtx, dst io.Writer, src io.Reader, wg *sync.WaitGroup)
 }
 
 func copyAndClose(ctx *ProxyCtx, dst, src halfClosable) {
+	if dst == nil || src == nil {
+		return
+	}
 	if _, err := io.Copy(dst, src); err != nil {
 		ctx.Warnf("Error copying to client: %s", err)
 	}
